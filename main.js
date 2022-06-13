ff5728509dd3ee0b997f68b632149e58ed5617c5
CodinGitHub <gms2programador@gmail.com>
2022-06-12 22:12:08 -0500

    script for video

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const form = document.querySelector('.container__content__form');
-const input = document.querySelector('.container__content__form__text');
-const error = document.querySelector('.container__content__form__error');
+const inputEmail = document.querySelector('.container__content__form__email');
+const error = document.querySelector('.container__content__form__email--error');
 
 form.addEventListener('submit', (event)=>{
     const regExp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
 
-    if(regExp.test(input.value)){
-        input.style.border = '1px solid hsl(0, 36%, 70%)';
-        input.style.backgroundImage = 'url("")'
+    if(regExp.test(inputEmail.value)){
+        inputEmail.style.border = '1px solid hsl(0, 36%, 70%)';
+        inputEmail.style.backgroundImage = 'url("")'
         error.innerHTML = '';
     }else{
         event.preventDefault();
-        input.style.border = '1px solid red';
-        input.style.backgroundImage = 'url("./images/icon-error.svg")';
+        inputEmail.style.border = '1px solid red';
+        inputEmail.style.backgroundImage = 'url("./images/icon-error.svg")';
         error.innerHTML = 'Please provide a valid email';
     }
 });
